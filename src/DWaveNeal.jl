edbe5dad4eb65ae11e7de51d878bb79ac9e2c877
Pedro Maciel Xavier <pedroxavier@psr-inc.com>
2023-01-07 10:57:11 -0300

    Update `Anneal -> 0.6.0`, `dwave-neal -> 0.6.0`

--- src/DWaveNeal.jl
+++ src/DWaveNeal.jl
@@ -1,6 +1,6 @@
 module DWaveNeal
 
-using Anneal
+using Anneal # Exports MOI
 using PythonCall
 
 # -*- :: Python D-Wave Simulated Annealing :: -*- #
@@ -10,25 +10,7 @@ function __init__()
     PythonCall.pycopy!(neal, pyimport("neal"))
 end
 
-Anneal.@anew Optimizer begin
-    name       = "D-Wave Neal Simulated Annealing Sampler"
-    sense      = :min
-    domain     = :bool
-    version    = v"0.5.9"
-    attributes = begin
-        "num_reads"::Integer                                = 1_000
-        "num_sweeps"::Integer                               = 1_000
-        "num_sweeps_per_beta"::Integer                      = 1
-        "beta_range"::Union{Tuple{Float64,Float64},Nothing} = nothing
-        "beta_schedule"::Union{Vector,Nothing}              = nothing
-        "beta_schedule_type"::String                        = "geometric"
-        "seed"::Union{Integer,Nothing}                      = nothing
-        "initial_states_generator"::String                  = "random"
-        "interrupt_function"::Union{Function,Nothing}       = nothing
-    end
-end
-
-const PARAM_LIST = [
+const _DWAVE_NEAL_ATTR_LIST = [
     :num_reads,
     :num_sweeps,
     :num_sweeps_per_beta,
@@ -40,40 +22,46 @@ const PARAM_LIST = [
     :interrupt_function,
 ]
 
+Anneal.@anew Optimizer begin
+    name       = "D-Wave Neal Simulated Annealing Sampler"
+    sense      = :min
+    domain     = :bool
+    version    = v"0.6.0"
+    attributes = begin
+        "num_reads"::Integer = 1_000
+        "num_sweeps"::Integer = 1_000
+        "num_sweeps_per_beta"::Integer = 1
+        "beta_range"::Union{Tuple{Float64,Float64},Nothing} = nothing
+        "beta_schedule"::Union{Vector,Nothing} = nothing
+        "beta_schedule_type"::String = "geometric"
+        "seed"::Union{Integer,Nothing} = nothing
+        "initial_states_generator"::String = "random"
+        "interrupt_function"::Union{Function,Nothing} = nothing
+    end
+end
+
+@doc raw"""
+    DWaveNeal.Optimizer{T}()
+
+D-Wave's Simulated Annealing Sampler for QUBO and Ising models.
+""" Optimizer
+
 function Anneal.sample(sampler::Optimizer{T}) where {T}
     # ~*~ Retrieve Ising Model ~*~ #
-    Q, α, β = Anneal.qubo(sampler, Dict, T)
+    Q, α, β = Anneal.qubo(sampler, Dict)
 
     # ~*~ Retrieve Optimizer Attributes ~*~ #
     params = Dict{Symbol,Any}(
-        param => MOI.get(
-            sampler,
-            MOI.RawOptimizerAttribute(string(param))
-        )
-        for param in PARAM_LIST
+        param => MOI.get(sampler, MOI.RawOptimizerAttribute(string(param)))
+        for param in _DWAVE_NEAL_ATTR_LIST
     )
 
     # ~*~ Call D-Wave Neal API ~*~ #
-    results = @timed neal_sample(Q, α, β; params...)
-    samples = results.value
-
-    # ~*~ Timing Information ~*~ #
-    time_data = Dict{String,Any}(
-        "effective" => results.time
-    )
-
-    # ~*~ Write metadata ~*~ #
-    metadata = Dict{String,Any}(
-        "time"   => time_data,
-        "origin" => "D-Wave Neal"
-    )
-
-    return Anneal.SampleSet{T}(samples, metadata)
-end
-
-function neal_sample(Q::Dict{Tuple{Int,Int},T}, α::T, β::T; params...) where {T}
     sampler = neal.SimulatedAnnealingSampler()
-    records = sampler.sample_qubo(Q; params...).record
+    results = @timed sampler.sample_qubo(Q; params...)
+    records = results.value.record
+
+    # ~*~ Format Samples ~*~ #
     samples = [
         Anneal.Sample{T,Int}(
             # state:
@@ -81,12 +69,19 @@ function neal_sample(Q::Dict{Tuple{Int,Int},T}, α::T, β::T; params...) where {
             # value: 
             α * (pyconvert(T, λ) + β),
             # reads:
-            pyconvert(Int, r),        
-        )
-        for (ψ, λ, r) in records
+            pyconvert(Int, r),
+        ) for (ψ, λ, r) in records
     ]
-    
-    return samples
+
+    # ~*~ Write metadata ~*~ #
+    metadata = Dict{String,Any}(
+        "origin" => "D-Wave Neal",
+        "time"   => Dict{String,Any}(
+            "effective" => results.time
+        ),
+    )
+
+    return Anneal.SampleSet{T}(samples, metadata)
 end
 
 end # module
